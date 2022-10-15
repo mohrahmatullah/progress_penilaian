955e803270e0d16828a203407dc5abee349c24b2
DEVELOP\rahme <mohrahmatullah17051992@gmail.com>
2022-10-15 11:27:38 +0700

    update

--- index.php
+++ index.php
@@ -106,14 +106,6 @@
 		    </div>
       </div>
     </div>
-    <div class="toast align-items-center text-white bg-primary border-0" role="alert" aria-live="assertive" aria-atomic="true">
-		  <div class="d-flex">
-		    <div class="toast-body">
-		      Hello, world! This is a toast message.
-		    </div>
-		    <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
-		  </div>
-		</div>
    </body>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
